24ff393a697dcfa81e36168165481198fbdc3282
Efren Rodriguez <1717110246@utectulancingo.edu.mx>
2021-05-26 10:52:19 -0500

    Nombre del proyecto my-app, Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/efren/myapp/entity/test.java
@@ -0,0 +1,23 @@
+package com.efren.myapp.entity;
+
+public class test {
+	private String nombre;
+	/*
+	 * prueba para la muestra de compilacion de archivo test
+	 */
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
